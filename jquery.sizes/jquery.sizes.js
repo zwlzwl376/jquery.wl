bbf3eea0789ae88b5169847fa8a5ba88e52f2644
ALong <zengweilong@vipkid.com.cn>
2015-12-24 14:19:03 +0800

    修正js 的字符计算

--- jquery.sizes/jquery.sizes.js
+++ jquery.sizes/jquery.sizes.js
@@ -10,11 +10,10 @@
 			content:'...'
 		};
 		json = $.extend(json,dfault, options);
-		var text = $(this).html();
+		var text = $.trim($(this).html());
 		var size = text.length;
 		if(size > json.size){
 			var jclen = json.content.length;
-			jclen--;
 			text = text.substring(0,(json.size-jclen)) + json.content;
 		}
 		$(this).html(text);
