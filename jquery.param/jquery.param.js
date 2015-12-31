661814769f792109d2d5317fbc8ea78c59ae0c5a
ALong <zengweilong@vipkid.com.cn>
2015-12-31 16:42:09 +0800

    新增URL参数获取通过正则表达式匹配

---
+++ jquery.param/jquery.param.js
@@ -0,0 +1,9 @@
+(function() {
+	$.getParam = function(name){
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) 
+			return decodeURIComponent(r[2]); 
+		return null;
+	};
+})();
